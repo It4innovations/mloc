464744abb35cba3100043c27f1d8bef1fd9ce47d
Vojtech Cima <cima.vojtech@gmail.com>
2018-10-02 14:00:39 +0200

    [test] Added test environment

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from multiprocessing import Process
+import pytest
+
+PYTEST_DIR = os.path.dirname(__file__)
+ROOT = os.path.dirname(os.path.dirname(PYTEST_DIR))
+MLOC_DIR = os.path.join(ROOT, "mloc")
+
+sys.path.insert(0, MLOC_DIR)
+
+from mloc.mloc import run_mloc  # noqa
+
+
+class Env:
+    def __init__(self):
+        self.p = Process(target=run_mloc)
+
+    def start(self):
+        self.p.start()
+
+    def stop(self):
+        if not self.p:
+            return
+        self.p.terminate()
+        self.p = None
+
+
+@pytest.yield_fixture(autouse=True, scope="function")
+def mloc_env():
+    env = Env()
+    yield env
+    env.stop()
