6be4176e456a3ef67bcd3310ae9493ea6df185cf
Vojtech Cima <cima.vojtech@gmail.com>
2018-10-03 13:02:23 +0200

    [test] Added test for convolutional neural net

--- tests/test_api.py
+++ tests/test_api.py
@@ -90,3 +90,85 @@ def test_dense_net_complex(mloc_env):
             break
 
     mloc_env.stop()
+
+
+def test_conv_net_complex(mloc_env):
+    mloc_env.start()
+
+    endpoint = 'users'
+    url = 'http://{}:{}/{}'.format(API_IP, API_PORT, endpoint)
+    data = {'username': str(uuid.uuid4().hex), 'password': 'secret'}
+    r = requests.post(url, json=data)
+    assert r.json()['_status'] == 'OK'
+
+    endpoint = 'login'
+    url = 'http://{}:{}/{}'.format(API_IP, API_PORT, endpoint)
+    r = requests.post(url, json=data)
+
+    headers = {'Authorization': 'token {}'.format(r.json()['token'])}
+
+    endpoint = 'networks'
+    url = 'http://{}:{}/{}'.format(API_IP, API_PORT, endpoint)
+    path = os.path.join(ROOT, 'examples', 'conv', 'convnet.json')
+    data = json.load(open(path, 'r'))
+    r = requests.post(url, json=data, headers=headers)
+    assert r.json()['_status'] == 'OK'
+    network = r.json()
+
+    endpoint = 'fits'
+    url = 'http://{}:{}/{}'.format(API_IP, API_PORT, endpoint)
+    path = os.path.join(ROOT, 'examples', 'conv', 'convfit.json')
+    data = json.load(open(path, 'r'))
+    data['network_id'] = network['_id']
+    r = requests.post(url, json=data, headers=headers)
+    assert r.json()['_status'] == 'OK'
+    fit = r.json()
+
+    while True:
+        time.sleep(1)
+        print('Waiting for model being created.')
+        endpoint = 'fits'
+        url = 'http://{}:{}/{}/{}'.format(
+            API_IP, API_PORT, endpoint, fit['_id'])
+        r = requests.get(url, headers=headers)
+        if r.json()['state'] == 'finished':
+            break
+
+    endpoint = 'evaluations'
+    url = 'http://{}:{}/{}'.format(API_IP, API_PORT, endpoint)
+    path = os.path.join(ROOT, 'examples', 'conv', 'conveval.json')
+    data = json.load(open(path, 'r'))
+    data['fit_id'] = fit['_id']
+    r = requests.post(url, json=data, headers=headers)
+    assert r.json()['_status'] == 'OK'
+    evaluation = r.json()
+
+    while True:
+        time.sleep(1)
+        endpoint = 'evaluations'
+        url = 'http://{}:{}/{}/{}'.format(
+            API_IP, API_PORT, endpoint, evaluation['_id'])
+        r = requests.get(url, headers=headers)
+        if r.json()['state'] == 'finished':
+            break
+
+    endpoint = 'predictions'
+    url = 'http://{}:{}/{}'.format(API_IP, API_PORT, endpoint)
+    path = os.path.join(ROOT, 'examples', 'conv', 'convpredict.json')
+    data = json.load(open(path, 'r'))
+    data['fit_id'] = fit['_id']
+    r = requests.post(url, json=data, headers=headers)
+    assert r.json()['_status'] == 'OK'
+    prediction = r.json()
+
+    while True:
+        time.sleep(1)
+        endpoint = 'predictions'
+        url = 'http://{}:{}/{}/{}'.format(
+            API_IP, API_PORT, endpoint, prediction['_id'])
+        r = requests.get(url, headers=headers)
+        print(r.json())
+        if r.json()['state'] == 'finished':
+            break
+
+    mloc_env.stop()
